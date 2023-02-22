d548cde2950bdc553a145682e97db6d71399f32e
Pursharth Voha <pursharth.vohra@gmail.com>
2023-02-22 12:15:55 -0500

    add metrial to project

--- src/script.js
+++ src/script.js
@@ -3,14 +3,17 @@ import gsap from "gsap";
 import GUI from "lil-gui";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
+// Textures
+const textureLoader = new THREE.TextureLoader();
+
 // Debug
 const gui = new GUI();
-const params = {
-    color: 0xff0000,
-    spin: () => {
-        gsap.to(mesh.rotation, { y: mesh.rotation.y + 8, duration: 2 });
-    },
-};
+// const params = {
+//     color: 0xff0000,
+//     spin: () => {
+//         gsap.to(mesh.rotation, { y: mesh.rotation.y + 8, duration: 2 });
+//     },
+// };
 
 // Cursor
 const cursor = {
@@ -31,29 +34,69 @@ const scene = new THREE.Scene();
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: params.color });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+//const material = new THREE.MeshNormalMaterial();
+//const material = new THREE.MeshLambertMaterial();
+//const material = new THREE.MeshPhongMaterial();
+//material.shininess = 50;
+//material.specular = new THREE.Color(0x1188ff);
+
+//const material = new THREE.MeshToonMaterial();
+const material = new THREE.MeshStandardMaterial();
+material.metalness = 0.45;
+material.roughness = 0.65;
+
+const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 16, 16), material);
+sphere.position.x = -1.5;
+sphere.geometry.setAttribute(
+    "uv2",
+    new THREE.BufferAttribute(sphere.geometry.attributes.uv.array, 2)
+);
+
+const plane = new THREE.Mesh(new THREE.PlaneGeometry(1, 1), material);
+const art = plane.geometry.setAttribute(
+    "uv2",
+    new THREE.BufferAttribute(plane.geometry.attributes.uv.array, 2)
+);
+
+const torus = new THREE.Mesh(
+    new THREE.TorusGeometry(0.3, 0.2, 16, 32),
+    material
+);
+torus.geometry.setAttribute(
+    "uv2",
+    new THREE.BufferAttribute(torus.geometry.attributes.uv.array, 2)
+);
+torus.position.x = 1.5;
+
+scene.add(sphere, plane, torus);
+
+// Lights
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+
+const pointLight = new THREE.PointLight(0xffffff, 0.5);
+pointLight.position.x = 2;
+pointLight.position.y = 3;
+pointLight.position.z = 4;
+scene.add(pointLight);
 
 // Debug
-gui.add(mesh.position, "y").min(-3).max(3).step(0.01).name("elevation");
-gui.add(mesh, "visible");
-gui.add(material, "wireframe");
-gui.addColor(params, "color").onChange(() => {
-    material.color.set(params.color);
-});
-gui.add(params, "spin");
+// gui.add(mesh.position, "y").min(-3).max(3).step(0.01).name("elevation");
+// gui.add(mesh, "visible");
+gui.add(material, "metalness").min(0).max(1).step(0.0001);
+gui.add(material, "roughness").min(0).max(1).step(0.0001);
+// gui.addColor(params, "color").onChange(() => {
+//     material.color.set(params.color);
+// });
+// gui.add(params, "spin");
 
 // Scale
-mesh.scale.x = 2;
 
 // Axes helper
 const axeshelper = new THREE.AxesHelper();
 //scene.add(axeshelper);
 
 // Rotation
-mesh.rotation.y = 0.5;
 
 /**
  * Sizes
@@ -104,8 +147,6 @@ const camera = new THREE.PerspectiveCamera(
 camera.position.z = 5;
 scene.add(camera);
 
-camera.lookAt(mesh.position);
-
 //Controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
@@ -121,14 +162,21 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 renderer.render(scene, camera);
 
 // Clock
-//const clock = new THREE.Clock();
+const clock = new THREE.Clock();
 
 // Animations
 const tick = () => {
     // Clock
-    //const elapsedTime = clock.getElapsedTime();
+    const elapsedTime = clock.getElapsedTime();
 
     //update object
+    sphere.rotation.y = 0.1 * elapsedTime;
+    plane.rotation.y = 0.1 * elapsedTime;
+    torus.rotation.y = 0.1 * elapsedTime;
+
+    sphere.rotation.x = 0.15 * elapsedTime;
+    plane.rotation.x = 0.15 * elapsedTime;
+    torus.rotation.x = 0.15 * elapsedTime;
     //mesh.position.x = Math.sin(elapsedTime);
     //mesh.position.y = Math.cos(elapsedTime);
 
